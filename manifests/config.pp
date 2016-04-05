5ddeb8d5332a1dbc749a67a2d246b9d54ac8ec27
Andreas Ntaflos <andreas.ntaflos@rise-world.com>
2016-04-05 16:58:52 +0200

    Support setting crashkernel size on Ubuntu 14.04
    In Ubuntu 14.04 the kexec-tools package installs a config file
    /etc/default/grub.d/kexec-tools.cfg, which is the right place to set a
    proper crashkernel size. This file takes precedence over, and thus
    shadows any changes made previously in, /etc/grub.d/10_linux.
    
    Unfortunately the default crashkernel size is still way too small for
    systems with more than a gigabyte of RAM so we still need to go in and
    fix it.

--- manifests/config.pp
+++ manifests/config.pp
@@ -32,19 +32,44 @@ class crashdump::config {
     }
     'Debian' : {
       case $::operatingsystem {
-        # This is a hack. We make sure that on Ubuntu the crashkernel line in
-        # /etc/grub.d/10_linux isn't something that will just NOT work, see errata:
-        #   https://wiki.ubuntu.com/Kernel/CrashdumpRecipe#Release_specific_notes
-        #
-        # Rationale: We could put this into /etc/default/grub but that would mean
-        # potentially messing with other people's grub config. Since there's no
-        # way of managing a single part of a single line in a configuration file
-        # or any sensible hooks for grub that everyone uses, we keep it simple.
         'Ubuntu' : {
-          file_line { 'crashkernel_size':
-            path  => '/etc/grub.d/10_linux',
-            match => '    GRUB_CMDLINE_EXTRA="\$GRUB_CMDLINE_EXTRA crashkernel=.*',
-            line  => "    GRUB_CMDLINE_EXTRA=\"\$GRUB_CMDLINE_EXTRA crashkernel=${crashkernel_size}\"",
+          case $::lsbdistrelease {
+            '12.04' : {
+              # This is a hack. We make sure that on Ubuntu the crashkernel line in
+              # /etc/grub.d/10_linux isn't something that will just NOT work, see errata:
+              #   https://wiki.ubuntu.com/Kernel/CrashdumpRecipe#Release_specific_notes
+              #
+              # Rationale: We could put this into /etc/default/grub but that would mean
+              # potentially messing with other people's grub config. Since there's no
+              # way of managing a single part of a single line in a configuration file
+              # or any sensible hooks for grub that everyone uses, we keep it simple.
+              file_line { 'crashkernel_size':
+                path  => '/etc/grub.d/10_linux',
+                match => '    GRUB_CMDLINE_EXTRA="\$GRUB_CMDLINE_EXTRA crashkernel=.*',
+                line  => "    GRUB_CMDLINE_EXTRA=\"\$GRUB_CMDLINE_EXTRA crashkernel=${crashkernel_size}\"",
+              }
+            }
+            '14.04' : {
+              # On Ubuntu 14.04 the kexec-tools package installs
+              # /etc/default/grub.d/kexec-tools.cfg. This is the place to set
+              # the crashkernel size. The default size is *still* way too small
+              # for systems with more than a gigabyte of RAM, leading to the
+              # crash kernel crashing. See
+              # https://wiki.ubuntu.com/Kernel/CrashdumpRecipe#Release_specific_notes
+              # for more.
+              file { '/etc/default/grub.d/kexec-tools.cfg':
+                ensure  => 'present',
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0644',
+                content => template("${module_name}/kexec-tools.cfg.erb"),
+              }
+            }
+            default: {
+              # Nothing yet, but if Ubuntu 16.04 works the same as 14.04 then we
+              # will manage /etc/default/grub.d/kexec-tools.cfg here for both
+              # 14.04 and 16.04.
+            }
           }
         }
         default : {
@@ -56,6 +81,4 @@ class crashdump::config {
       # nothing to do here.
     }
   }
-
-
 }
